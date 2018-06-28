c4392dbe7b83589bd94a1cf72bdfddbdb912ccfb
hub2016 <896939236@qq.com>
2018-06-28 17:56:44 +0800

    生活指数显示建议明细

--- pages/index/index.js
+++ pages/index/index.js
@@ -57,7 +57,6 @@ function CalConvert(date) {
     t += "月"
     t += (total < 11) ? "初" : ((total < 20) ? "十" : ((total < 30) ? "廿" : "卅"))
     if (total % 10 !== 0 || total === 10) t += "一二三四五六七八九十".charAt((total - 1) % 10)
-    console.log(t)
     return t
   }
   return func(date)
@@ -79,6 +78,30 @@ Page({
     locationAuthType: UNPROMPTED
   },
   onLoad: function () {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+    })
+
     wx.getSetting({
       success: res => {
         let auth = res.authSetting['scope.userLocation']
@@ -101,6 +124,12 @@ Page({
       ak: 'Ygj6o0ERIEn7zHGziz6O2xUvu9iRClxa'
     });
   },
+  onShareAppMessage: function () {
+    return {
+      title: '实时及未来三天天气预测',
+      path: '/pages/index/index'
+    }
+  },
   onTapLocation() {
     if (this.data.locationAuthType === UNAUTHORIZED)
       wx.openSetting({
@@ -110,8 +139,6 @@ Page({
           }
         }
       })
-    //else
-      //this.getNow()
   },
   onPullDownRefresh() {
     this.getNow(() => {
